1dcccae3c7c5327cad9b1302f76dc088f21b40a5
chanakaHetti <cdileep40@gmail.com>
2024-10-20 23:59:33 +0100

    Adding the string.Trim() method

--- Program.cs
+++ Program.cs
@@ -1,1 +1,10 @@
 ﻿Console.WriteLine("Hello, World!");
+
+// String Trim()
+
+string firstName = "       Chanaka     ";
+firstName = firstName.Trim();
+
+string lastName = "         HA      ";
+
+Console.WriteLine($"My name is: {firstName} {lastName.Trim()}");
