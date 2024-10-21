0b421420ca7f1da033161d0beb366b21d61f4d20
chanakaHetti <cdileep40@gmail.com>
2024-10-21 20:16:43 +0100

    Handling the numbers

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿Console.WriteLine("Hello, World!");
 
-// String methods
+// String and String methods
+Console.WriteLine("----------- String and String methods -----------");
 
 string firstName = "       Chanaka     ";
 firstName = firstName.Trim();
@@ -23,3 +24,28 @@ Console.WriteLine(fullName.Contains("chanaka"));
 Console.WriteLine(fullName.ToUpper());
 
 Console.WriteLine(fullName.Length);
+
+// Numbers and number methods
+Console.WriteLine("----------- Numbers and number methods -----------");
+
+int a = 18;
+int b = 6;
+int c = a + b;
+
+Console.WriteLine(c);
+
+Console.WriteLine("Long number (More then 2.1 billion) -------------");
+
+int d = 2100000000;
+int e = 2100000000;
+long f = d + e; // Long isn't work here too. Result os too long
+
+Console.WriteLine(f);
+
+long g = (long)d + (long)e;
+
+Console.WriteLine(g);
+
+// long h = checked(d + e);
+
+// Console.WriteLine(h);
